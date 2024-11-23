c477bfa35e6e7c4fd11e1fe2a5f95aa2813fa6f0
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-11-22 20:49:19 -0500

    timestamps added in inventory model

--- backend/models/inventory.js
+++ backend/models/inventory.js
@@ -2,40 +2,45 @@
 
 const mongoose = require("mongoose");
 
-const inventorySchema = new mongoose.Schema({
-  restaurantName: {
-    type: String,
-    required: true,
+const inventorySchema = new mongoose.Schema(
+  {
+    restaurantName: {
+      type: String,
+      required: true,
+    },
+    restaurantId: {
+      type: String,
+      required: true,
+    },
+    itemName: {
+      type: String,
+      required: true,
+    },
+    metric: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      default: 0,
+    },
+    costperitem: {
+      type: Number,
+      required: true,
+    },
+    datebought: {
+      type: Date,
+      required: true,
+    },
+    dateexpired: {
+      type: Date,
+      required: true,
+    },
   },
-  restaurantId: {
-    type: String,
-    required: true,
-  },
-  itemName: {
-    type: String,
-    required: true,
-  },
-  metric: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    default: 0,
-  },
-  costperitem: {
-    type: Number,
-    required: true,
-  },
-  datebought: {
-    type: Date,
-    required: true,
-  },
-  dateexpired: {
-    type: Date,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Inventory = mongoose.model("Inventory", inventorySchema);
 
