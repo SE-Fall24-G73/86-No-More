fc6e0d7b5f2d978bdbbe06eef36d66a946420bea
AakashBhandary <aakashbhandary2024@gmail.com>
2024-11-24 23:31:21 -0500

    added function to handle image uploads

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ import tempfile
 import google.generativeai as genai
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -14,3 +15,10 @@ CORS(app)
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 
+def upload_to_gemini(file_storage, mime_type=None):
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        file_storage.save(tempfile.name)
+        file = genai.upload_file(temp_file.name, mime_type=mime_type)
+    os.remove(temp_file.name)
+    print(f"Uploaded the file '{file.display_name}' as: {file.uri}")
+    return file
