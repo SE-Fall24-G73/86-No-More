77f620f1d0086162893579c92952e3ad28cb6d0e
AakashBhandary <aakashbhandary2024@gmail.com>
2024-11-25 23:25:25 -0500

    Added frontend for user ratings

--- frontend/src/components/Ratings.js
+++ frontend/src/components/Ratings.js
@@ -1,99 +1,135 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import Rating from '@material-ui/lab/Rating';
-class Ratings extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            restname: '',
-            restid: '',
-            menuname: '',
-            quantity: '0',
-            costmenu: '',
-            cartTotal: 0,
-            setInput: '0',
-            cartChange: '0',
-            editMode: false,
-            star:0
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import Rating from '@material-ui/lab/Rating'
+import '../styles/Ratings.css'
+import { APIURLS } from '../helpers/urls'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+
+const Ratings = () => {
+    const [ratings, setRatings] = useState({})
+    const [loadingStates, setLoadingStates] = useState({})
+    const [menu, setMenu] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    const fetchAllMenu = async () => {
+        try {
+            setLoading(true)
+            const data = await axios.get(APIURLS.fetchAllMenus())
+            setMenu(data.data.data.menu)
+        } catch (error) {
+            console.log('error', error)
+            toast.error('Failed to fetch menu')
+        } finally {
+            setLoading(false)
         }
     }
-    handleInputChange = (fieldName, val) => {
-        this.setState({
-            [fieldName]: val,
-        })
+
+    useEffect(() => {
+        fetchAllMenu()
+    }, [])
+
+    // Handler for rating changes
+    const handleRatingChange = (menuItemId, newValue) => {
+        setRatings((prevRatings) => ({
+            ...prevRatings,
+            [menuItemId]: newValue,
+        }))
     }
-    render(){
-        const { menu } = this.props
-        return(
-            <div>
-                <div style={{ marginLeft: '57px' }}>
-                    {menu?.map((menu) => (
-                       <div
-                       className="post"
-                       key={menu.restname}
-                       style={{ width: '50vw', marginLeft: '50px' }}
-                   >
-                    {
-                        (1>0)?(
-                            <div className="post-header">
-                                <div>
-                                    <h4 style={{ display: 'inline-block' }}>
-                                        Restaurant Name :{' '}
-                                    </h4>
-                                    <span style={{ marginLeft: '10px' }}>
-                                        {menu.restname}
-                                    </span>
-                                    <Rating style={{marginLeft:"300px"}} name="half-rating" 
-                                    defaultValue={menu.star} 
-                                    precision={0.5} 
-                                    onChange={(e)=>{this.handleInputChange('star', e.target.value)}}
-                                    />
-                                    
-                                </div>
-                                <div>
-                                    <h4
-                                        style={{
-                                            display: 'inline-block',
-                                            marginTop: '-12px',
-                                        }}
-                                    >
-                                        Dish :{' '}
-                                    </h4>
-                                    <span style={{ marginLeft: '10px' }}>
-                                        {menu.menuname}
-                                    </span>
-                                </div>
-                                <div>
-                                    <h4
-                                        style={{
-                                            display: 'inline-block',
-                                            marginTop: '-12px',
-                                        }}
-                                    >
-                                        Price :{' '}
-                                    </h4>
-                                    <span style={{ marginLeft: '10px' }}>
-                                        {menu.costmenu}
-                                    </span>
-                                </div>
-                                <div>
-                                </div>
+
+    const handleSubmit = async (menuItemId) => {
+        const ratingValue = ratings[menuItemId]
+        if (!ratingValue) {
+            toast.error('Please select a rating before submitting.')
+            return
+        }
+
+        setLoadingStates((prevLoading) => ({
+            ...prevLoading,
+            [menuItemId]: true,
+        }))
+
+        try {
+            const response = await axios.post(APIURLS.submitRating(), {
+                foodItemId: menuItemId,
+                rating: ratingValue,
+            })
+
+            toast.success('Rating submitted successfully!')
+
+            setRatings((prevRatings) => ({
+                ...prevRatings,
+                [menuItemId]: 0,
+            }))
+        } catch (error) {
+            console.error('Error submitting rating:', error)
+            toast.error('Failed to submit rating. Please try again.')
+        } finally {
+            setLoadingStates((prevLoading) => ({
+                ...prevLoading,
+                [menuItemId]: false,
+            }))
+        }
+    }
+
+    return (
+        <div className="ratings-container">
+            <div className="ratings-content">
+                {menu?.map((menuItem) => (
+                    <div
+                        className="post"
+                        key={menuItem._id}
+                        style={{ width: '50vw', margin: '20px auto' }}
+                    >
+                        <div className="post-header">
+                            <div className="restaurant-info">
+                                <h4 className="label">Restaurant Name:</h4>
+                                <span className="value">
+                                    {menuItem.restaurantName}
+                                </span>
                             </div>
-                        ):null
-                    }
-                   </div>
-                    ))}
-                </div>
+                            <div className="rating-section">
+                                <Rating
+                                    className="rating-component"
+                                    name={`rating-${menuItem._id}`}
+                                    value={ratings[menuItem._id] || 0}
+                                    precision={0.5}
+                                    onChange={(event, newValue) => {
+                                        handleRatingChange(
+                                            menuItem._id,
+                                            newValue
+                                        )
+                                    }}
+                                />
+                                <button
+                                    className="submit-button"
+                                    onClick={() => handleSubmit(menuItem._id)}
+                                    disabled={
+                                        !ratings[menuItem._id] ||
+                                        loadingStates[menuItem._id]
+                                    }
+                                >
+                                    {loadingStates[menuItem._id]
+                                        ? 'Submitting...'
+                                        : 'Submit'}
+                                </button>
+                            </div>
+                            <div className="dish-info">
+                                <h4 className="label">Dish:</h4>
+                                <span className="value">
+                                    {menuItem.itemName}
+                                </span>
+                            </div>
+                            <div className="price-info">
+                                <h4 className="label">Price:</h4>
+                                <span className="value">{menuItem.cost}</span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-        )
-    }
+        </div>
+    )
 }
-function mapStateToProps(state) {
-    return {
-        auth: state.auth,
-        results: state.search.results,
-        job: state.job,
-        menu: state.menu,
-    }
-}
-export default connect(mapStateToProps)(Ratings)
+
+export default Ratings
