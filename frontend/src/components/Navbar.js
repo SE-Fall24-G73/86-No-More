4e8566d64e9bab663634746e8429bded8167ae04
Vaishvi Shah <vaishvi.sce20@sot.pdpu.ac.in>
2024-11-25 20:49:30 -0500

    Updated Navbar.js file

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,280 +1,187 @@
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import React from 'react'
-import { logoutUser } from '../actions/auth'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logoutUser } from '../actions/auth';
+import { searchUsers } from '../actions/search';
+import CameraIndoorIcon from '@mui/icons-material/CameraIndoor';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import StarIcon from '@material-ui/icons/Star';
+import RestaurantMenuIcon from '@material-ui/icons/RestaurantMenu';
+import InventoryIcon from '@material-ui/icons/InsertDriveFile';
+import TimelineIcon from '@material-ui/icons/Timeline';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
-import { searchUsers } from '../actions/search'
-import CameraIndoorIcon from '@mui/icons-material/CameraIndoor'
-import ListAltIcon from '@material-ui/icons/ListAlt'
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
-import StarIcon from '@material-ui/icons/Star'
-import RestaurantMenuIcon from '@material-ui/icons/RestaurantMenu'
-import InventoryIcon from '@material-ui/icons/InsertDriveFile'
-import TimelineIcon from '@material-ui/icons/Timeline'
-import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive'
+function Navbar(props) {
+    const [menuOpen, setMenuOpen] = useState(false);
 
-class Navbar extends React.Component {
-    logOut = () => {
-        localStorage.removeItem('token')
-        this.props.dispatch(logoutUser())
-    }
+    const logOut = () => {
+        localStorage.removeItem('token');
+        props.dispatch(logoutUser());
+    };
 
-    handleSearch = (e) => {
-        const searchText = e.target.value
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
-        this.props.dispatch(searchUsers(searchText))
-    }
+    const { auth } = props;
+    const { user } = auth;
 
-    render() {
-        const { auth } = this.props
-        const { user } = this.props.auth
+    return (
+        <nav className="header">
+            {/* Left Section */}
+            <div className="header__left">
+                <Link to="/">
+                    <img className='logo-nav'
+                        alt="logo"
+                        src="/images/WhatsApp Image 2021-12-04 at 4.31.50 PM.jpeg"
+                        style={{ height: '50px' }}
+                    />
+                </Link>
+            </div>
 
-        return (
-            <nav className="header">
-                <div
-                    className="header__left"
-                    style={{ display: 'flex', justifyContent: 'space-evenly' }}
-                >
-                    <Link to="/">
-                        <img
-                            alt="logo"
-                            src="/images/WhatsApp Image 2021-12-04 at 4.31.50 PM.jpeg"
-                            style={{ height: '50px' }}
-                        />
-                    </Link>
-                </div>
-                {auth.isLoggedIn && (
-                    <div
-                        className="header__middle"
-                        style={{ marginLeft: '20px' }}
-                    >
+            {/* Middle Section */}
+            <div className={`header__middle ${menuOpen ? 'show' : ''}`}>
+                {auth.isLoggedIn ? (
+                    <>
                         {user.role === 'owner' && (
                             <>
-                                <div
-                                    className="header__option "
-                                    style={{ display: 'block' }}
-                                >
+                                <div className="header__option">
                                     <Link to="/create-menu">
-                                        <RestaurantMenuIcon fontSize="large" />
+                                        <RestaurantMenuIcon fontSize="large" className="icon-red" />
                                     </Link>
-                                    <p style={{ marginTop: '0px' }}>
-                                        Create Menu
-                                    </p>
+                                    <p>Create Menu</p>
                                 </div>
-                                <div
-                                    className="header__option "
-                                    style={{ display: 'block' }}
-                                >
+                                <div className="header__option">
                                     <Link to="/cart">
                                         <ShoppingCartIcon fontSize="large" />
                                     </Link>
-                                    <p style={{ marginTop: '0px' }}>Cart</p>
+                                    <p>Cart</p>
                                 </div>
-                                <div
-                                    className="header__option "
-                                    style={{ display: 'block' }}
-                                >
+                                <div className="header__option">
                                     <Link to="/goal">
-                                        <ListAltIcon
-                                            fontSize="large"
-                                            style={{ marginLeft: '13px' }}
-                                        />
+                                        <ListAltIcon fontSize="large" />
                                     </Link>
-                                    <p style={{ marginTop: '0px' }}>
-                                        Inventory
-                                    </p>
+                                    <p>Inventory</p>
                                 </div>
-                                <div
-                                    className="header__option "
-                                    style={{ display: 'block' }}
-                                >
+                                <div className="header__option">
                                     <Link to="/update">
-                                        <InventoryIcon
-                                            fontSize="large"
-                                            style={{ marginLeft: '5px' }}
-                                        />
+                                        <InventoryIcon fontSize="large" />
                                     </Link>
-                                    <p style={{ marginTop: '0px' }}>Predict</p>
+                                    <p>Predict</p>
                                 </div>
-                                <div
-                                    className="header__option "
-                                    style={{ display: 'block' }}
-                                >
+                                <div className="header__option">
                                     <Link to="/history">
                                         <TimelineIcon fontSize="large" />
                                     </Link>
-                                    <p style={{ marginTop: '0px' }}>Graph</p>
+                                    <p>Graph</p>
                                 </div>
-                                <div
-                                    className="header__option "
-                                    style={{ display: 'block' }}
-                                >
+                                <div className="header__option">
                                     <Link to="/notification">
-                                        <NotificationsActiveIcon
-                                            fontSize="large"
-                                            style={{ marginLeft: '20px' }}
-                                        />
+                                        <NotificationsActiveIcon fontSize="large" />
                                     </Link>
-                                    <p style={{ marginTop: '0px' }}>
-                                        Notification
-                                    </p>
+                                    <p>Notification</p>
                                 </div>
                             </>
                         )}
 
                         {user.role === 'customer' && (
                             <>
-                                <div
-                                    className="header__option "
-                                    style={{ display: 'block' }}
-                                >
+                                <div className="header__option">
                                     <Link to="/menu">
                                         <RestaurantMenuIcon fontSize="large" />
                                     </Link>
-                                    <p style={{ marginTop: '0px' }}>Menu</p>
+                                    <p>Menu</p>
                                 </div>
-                                <div
-                                    className="header__option "
-                                    style={{ display: 'block' }}
-                                >
+                                <div className="header__option">
                                     <Link to="/ratings">
-                                        <StarIcon
-                                            fontSize="large"
-                                            style={{ marginLeft: '17px' }}
-                                        />
+                                        <StarIcon fontSize="large" />
                                     </Link>
-                                    <p style={{ marginTop: '0px' }}>Feedback</p>
+                                    <p>Feedback</p>
                                 </div>
-                                <div
-                                    className="header__option "
-                                    style={{ display: 'block' }}
-                                >
+                                <div className="header__option">
                                     <Link to="/awareness">
-                                        <CameraIndoorIcon
-                                            fontSize="large"
-                                            style={{ marginLeft: '17px' }}
-                                        />
+                                        <CameraIndoorIcon fontSize="large" />
                                     </Link>
-                                    <p style={{ marginTop: '0px' }}>
-                                        Awareness
-                                    </p>
+                                    <p>Awareness</p>
                                 </div>
-                                <div
-                                    className="header__option "
-                                    style={{ display: 'block' }}
-                                >
+                                <div className="header__option">
                                     <Link to="/caloryDetector">
-                                        <CameraIndoorIcon
-                                            fontSize="large"
-                                            style={{ marginLeft: '17px' }}
-                                        />
+                                        <FastfoodIcon fontSize="large" />
                                     </Link>
-                                    <p style={{ marginTop: '0px' }}>
-                                        Calory Detector
-                                    </p>
+                                    <p>Calory Detector</p>
                                 </div>
-                                <div
-                                    className="header__option "
-                                    style={{ display: 'block' }}
-                                >
+                                <div className="header__option">
                                     <Link to="/healthAdvisor">
-                                        <CameraIndoorIcon
-                                            fontSize="large"
-                                            style={{ marginLeft: '17px' }}
-                                        />
+                                        <HealthAndSafetyIcon fontSize="large" />
                                     </Link>
-                                    <p style={{ marginTop: '0px' }}>
-                                        Health Advisor
-                                    </p>
+                                    <p>Health Advisor</p>
                                 </div>
                             </>
                         )}
-                    </div>
-                )}
-                <div className="header__right">
-                    <div className="header__info">
-                        {auth.isLoggedIn && (
-                            <div className="user">
-                                <Link to="/settings">
-                                    <img
-                                        src="/images/contact-person-red-icon-free-png.png"
-                                        alt="user-dp"
-                                        id="user-dp"
-                                        style={{
-                                            marginLeft: '0px',
-                                            height: '30px',
-                                            width: '30px',
-                                        }}
-                                    />
-                                </Link>
-                                <span
-                                    style={{
-                                        color: 'gray',
-                                        marginLeft: '10px',
-                                        fontWeight: 'bolder',
-                                    }}
-                                >
-                                    {auth.user.name}
-                                </span>
-                            </div>
-                        )}
+                    </>
+                ):(<div className="auth-links">
+                    <Link to="/login">Login</Link>
+                    <Link to="/signup">Register</Link>
+                </div>)}
+            </div>
 
-                        <div className="nav-links">
-                            <ul>
-                                {!auth.isLoggedIn && (
-                                    <li>
-                                        <Link
-                                            to="/login"
-                                            style={{
-                                                color: 'gray',
-                                                marginLeft: '10px',
-                                                fontWeight: 'bolder',
-                                            }}
-                                        >
-                                            Login
-                                        </Link>
-                                    </li>
-                                )}
+            {/* Right Section */}
+            <div className="header__right">
+    {auth.isLoggedIn ? (
+        <>
+            <div className="user">
+                <Link to="/settings">
+                    <img
+                        src="/images/contact-person-red-icon-free-png.png"
+                        alt="user-dp"
+                        id="user-dp"
+                        style={{
+                            marginLeft: '0px',
+                            height: '30px',
+                            width: '30px',
+                        }}
+                    />
+                </Link>
+                <span
+                    style={{
+                        color: 'red',
+                        marginLeft: '10px',
+                        fontWeight: 'bolder',
+                    }}
+                >
+                    {auth.user.name}
+                </span>
+            </div>
+            <div className="header__option">
+                <Link to="#" onClick={logOut}>
+                    Logout
+                </Link>
+            </div>
+        </>
+    ) : (
+        <div className="auth-links large-screen">
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Register</Link>
+        </div>
+    )}
+    <div className="hamburger" onClick={toggleMenu}>
+        {menuOpen ? <CloseIcon fontSize="large" /> : <MenuIcon fontSize="large" />}
+    </div>
+</div>
 
-                                {auth.isLoggedIn && (
-                                    <li
-                                        onClick={this.logOut}
-                                        style={{
-                                            color: 'gray',
-                                            marginLeft: '0px',
-                                            fontWeight: 'bolder',
-                                        }}
-                                    >
-                                        Logout
-                                    </li>
-                                )}
-                                {!auth.isLoggedIn && (
-                                    <li>
-                                        <Link
-                                            to="/signup"
-                                            style={{
-                                                color: 'gray',
-                                                marginLeft: '10px',
-                                                fontWeight: 'bolder',
-                                            }}
-                                        >
-                                            Register
-                                        </Link>
-                                    </li>
-                                )}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        )
-    }
+        </nav>
+    );
 }
 
 function mapStateToProps(state) {
     return {
         auth: state.auth,
-    }
+    };
 }
 
-export default connect(mapStateToProps)(Navbar)
+export default connect(mapStateToProps)(Navbar);
